77dbd66b893d123a3b8b83badfe0a7b16e4f5a9d
Israt Jahan <israt5227@gmail.com>
2026-02-16 01:50:38 +0600

    task 3

--- practice-problem.js
+++ practice-problem.js
@@ -58,4 +58,26 @@ var total = math + biology + chemistry + physics + bangla;
 var avg = (total / 5);
 
 
-console.log(avg.toFixed(2));
+// console.log(avg.toFixed(2));
+
+/* 
+Task-3
+You task is to divide the given number by 5 and show the remainder as the output.
+
+Input:
+The first line of the input contains the number.
+
+Output:
+Print the remainder.
+
+Sample Input:
+119
+
+Sample Output:
+4
+*/
+// task 3 solution
+
+var num = 119;
+var remainder = num % 5;
+console.log(remainder)
